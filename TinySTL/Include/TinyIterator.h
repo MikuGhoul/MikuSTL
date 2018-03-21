11244236cacc82abfad59d9644e12b40cf62091b
MikuGhoul <mikughoull@gmail.com>
2018-03-21 12:59:00 +0800

    iterator trying

---
+++ TinySTL/Include/TinyIterator.h
@@ -0,0 +1,24 @@
+#ifndef TINYITERATOR_H__
+#define TINYITERATOR_H__
+
+namespace Miku {
+
+	template<class T>
+	class InputIterator {
+
+	};
+
+	template<class T>
+	class OutputIterator {
+
+	};
+
+	template<class T>
+	class RandomAccessIterator {
+
+	};
+	
+}
+
+
+#endif // !TINYITERATOR_H__
--- TinySTL/Include/TinyList.h
+++ TinySTL/Include/TinyList.h
@@ -1,25 +1,28 @@
-#ifndef TINYVECTOR_H__
-#define TINYVECTOR_H__
+#ifndef TINYLIST_H__
+#define TINYLIST_H__
 
 #include "TinyAllocator.h"
 #include "TinyAllocTraits.h"
-#include "TinyIterator.h"
 
 namespace Miku {
-	template<class T, class Allocator = Miku::allocator<T>>
-	class vector {
+	template<class T, class Allocator = Miku::allocator>
+	class list {
+
+	public:
 		using value_type = T;
 		using allocator_type = Allocator;
 		using size_type = std::size_t;
 		using difference_type = std::ptrdiff_t;
-		using reference = value_type & ;
-		using const_reference = const value_type & ;
+		using reference = value_type&;
+		using const_reference = const value_type&;
 		using pointer = Miku::allocator_traits<Allocator>::pointer;
 		using const_pointer = Miku::allocator_traits<Allocator>::const_pointer;
-		using iterator = 
+		
 
+	
 	};
 }
 
-#endif // !TINYVECTOR_H__
+
+#endif // !TINYLIST_H__
 
