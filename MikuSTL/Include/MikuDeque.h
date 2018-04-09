b7401dadc82410326dce4136d766d85cd23fa02e
MikuGhoul <mikughoull@gmail.com>
2018-04-09 18:47:14 +0800

    So many flags... // TODO

--- MikuSTL/Include/MikuDeque.h
+++ MikuSTL/Include/MikuDeque.h
@@ -17,33 +17,64 @@ namespace Miku {
 			using value_type = T;
 			using pointer = T * ;
 			using reference = T & ;
+			using const_reference = const T & ;
 			using size_type = typename std::size_t;
 			using difference_type = typename std::ptrdiff_t;
 
+			using self = _Deque_Iterator<T>;
+
 			using map_pointer = pointer * ;
 
-		
-			pointer first;		// buffer的头
-			pointer last;		// buffer的尾
-			pointer cur;		// 当前元素在buffer的位置
+		public:
+			pointer _B_First;		// buffer的头
+			pointer _B_Last;		// buffer的尾的后一个元素
+			pointer _B_Cur;			// 当前元素在buffer的位置
 
-			map_pointer node;	// buffer在"中控器"的位置
+			map_pointer _B_Node;	// buffer在map的位置
 
-		
-			_Deque_Iterator() : first(nullptr), last(nullptr), cur(nullptr), node(nullptr) {}
+			// TODO，没有设计好抽象策略...
+			// buffer长度分配策略
+			// 1. 指定为不为零的_Size
+			// 2. 若_Size为0，且每个元素小于512 byte，长度为 512 / sizeof(T)
+			// 3. 若_Size为0，且每个元素大于512 byte，长度为 1
+			static size_type _Deque_Buffer_Size(size_type _Size = 0) {
+				return _Size != 0 ? _Size : (sizeof(T) < 512 ? 512 / sizeof(T) : 1);
+			}
+
+		private:
+			void _Set_Node(map_pointer);
+
+		public:
+			_Deque_Iterator() : _B_First(nullptr), _B_Last(nullptr), _B_Cur(nullptr), _B_Node(nullptr) {}
 			_Deque_Iterator(size_type _index, pointer _cur, map_pointer _node) :
-				first(*_node), last(first + _index), cur(_cur), node(_node) {}
+				_B_First(*_node), _B_Last(_B_First + _index), _B_Cur(_cur), _B_Node(_node) {}
 			_Deque_Iterator(const _Deque_Iterator& other) :
-				first(other.first), last(other.last), cur(other.cur), node(other.node) {}
+				_B_First(other._B_First), _B_Last(other._B_Last), _B_Cur(other._B_Cur), _B_Node(other._B_Node) {}
 
 
-			reference operator*() const { return *cur; }
-			pointer operator->() const { return cur; }
+			reference operator*() const { return *_B_Cur; }
+			pointer operator->() const { return _B_Cur; }
+			self& operator++();
+			self operator++(int);
+			self& operator--();
+			self operator--(int);
+			difference_type operator-(const_reference) const;
 
 		};
 
+		template<class T>
+		bool operator==(const _Deque_Iterator<T>& lhs, const _Deque_Iterator<T>& rhs) {
+			return lhs._B_Cur == rhs._B_Cur;
+		}
+
+		template<class T>
+		bool operator!=(const _Deque_Iterator<T>& lhs, const _Deque_Iterator<T>& rhs) {
+			return (!(lhs == rhs));
+		}
+
 	}
 
+	// 实现设计：一段名为map的连续空间，存储指向buffer的指针，buffer中真正存放数据
 	template<class T, class Allocator = Miku::allocator<T>>
 	class deque {
 
@@ -62,6 +93,7 @@ namespace Miku {
 		using iterator = Internal::_Deque_Iterator<T>;
 
 	private:
+		// start和finish是有数据的buffer的头尾，不是map的头尾
 		iterator start;
 		iterator finish;
 		map_pointer map;
@@ -79,6 +111,9 @@ namespace Miku {
 		// ~deque();
 
 	private:
+		pointer _New_Buffer();
+		map_pointer _New_Map(const size_type);
+		void _Init_Map();
 
 	public:
 		iterator begin() { return start; }
--- MikuSTL/Include/MikuDeque.impl.h
+++ MikuSTL/Include/MikuDeque.impl.h
@@ -9,16 +9,86 @@ namespace Miku {
 		_Deque_Iterator<T>::_Deque_Iterator() {
 
 		}*/
+
+		template<class T>
+		void _Deque_Iterator<T>::_Set_Node(map_pointer _New_Node) {
+			_B_Node = _New_Node;
+			_B_First = *_New_Node;
+			_B_Last = _B_First + static_cast<difference_type>(_Deque_Buffer_Size());
+		}
+
+		template<class T>
+		typename _Deque_Iterator<T>::self& _Deque_Iterator<T>::operator++() {
+			++_B_Cur;
+			if (_B_Cur == _B_Last) {		// iterator到达当前buffer末尾
+				_Set_Node(_B_Node + 1);		// 切换至下一个buffer
+				_B_Cur = _B_First;			// iterator指向下一个buffer的起点的开头
+			}
+			return *this;
+		}
+
+		template<class T>
+		typename _Deque_Iterator<T>::self _Deque_Iterator<T>::operator++(int) {
+			self _temp = *this;
+			++*this;
+			return _temp;
+		}
+
+		template<class T>
+		typename _Deque_Iterator<T>::self& _Deque_Iterator<T>::operator--() {
+			if (_B_Cur == _B_First) {		// iterator已是当前buffer起点
+				_Set_Node(_B_Node - 1);		// 切换至上一个buffer
+				_B_Cur = _B_Last;			// iterator指向上一个buffer的末尾的后一个元素
+			}
+			--_B_Cur;
+			return *this;
+		}
+
+		template<class T>
+		typename _Deque_Iterator<T>::self _Deque_Iterator<T>::operator--(int) {
+			self _temp = *this;
+			--*this;
+			return _temp;
+		}
+
+		template<class T>
+		typename _Deque_Iterator<T>::difference_type _Deque_Iterator<T>::operator-(const_reference other) const {
+			// 每个buffer中的元素个数 * 两个iterator距离之间满数据的buffer + 两个iterator所在的buffer中元素剩余数
+			return difference_type(_Deque_Buffer_Size()) * (_B_Node - other._B_Node - 1) +
+				(_B_Cur - _B_First) + (other._B_Last - other._B_Cur);
+		}
+
+	}
+
+
+	template<class T, class Allocator>
+	typename deque<T, Allocator>::pointer deque<T, Allocator>::_New_Buffer() {
+		return allocator_type::allocate(iterator::_Deque_Buffer_Size());
 	}
 
 	template<class T, class Allocator>
-	deque<T, Allocator>::deque() : start(), finish(), map(nullptr), mapSize(0){
+	typename deque<T, Allocator>::map_pointer deque<T, Allocator>::_New_Map(const size_type _Map_Size) {
+		map_pointer _N_map = Miku::allocator<pointer>::allocate(_Map_Size);
+		for (int i = 0; i != _Map_Size; ++i)
+			_N_map[i] = _New_Buffer();
+		return _N_map;
+	}
+
+	template<class T, class Allocator>
+	void deque<T, Allocator>::_Init_Map() {
+		mapSize = 2;
+		map = _New_Map(mapSize);
+
+		// TODO
 
 	}
 
 	template<class T, class Allocator>
-	explicit deque<T, Allocator>::deque(size_type count, const_reference value) {
+	deque<T, Allocator>::deque() : start(), finish(), map(nullptr), mapSize(0){}
 
+	template<class T, class Allocator>
+	explicit deque<T, Allocator>::deque(size_type count, const_reference value) :deque() {
+		// 
 	}
 
 	template<class T, class Allocator>
